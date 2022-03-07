7580d76fc6df52da31ac6472334bd3314f2469e1
Nathan Wolf <aprilscout.dog@gmail.com>
2022-03-06 22:43:33 -0500

    Built new version of app for testing and Zip for sharing

--- dist/Interface/GlobalVars.py
+++ dist/Interface/GlobalVars.py
@@ -1,8 +1,15 @@
 import json
 from pathlib import Path
+import os
 
 homeDir = str(Path.home())  # homeDir, for example, is /Users/nathanwolf
 appSupportFolder = f'{homeDir}/Library/Application Support/FileBreakerApp'
+libraryPath = f'{homeDir}/Library'
+if not os.path.exists(libraryPath):
+    os.mkdir(libraryPath)
+appSupport = os.path.dirname(appSupportFolder)
+if not os.path.exists(appSupport):
+    os.mkdir(appSupport)
 currentChoicesPath = f'{appSupportFolder}/currentChoices.json'
 userInfoPath = f'{appSupportFolder}/userInfo.json'
 botChannelID = 939234547906777139
