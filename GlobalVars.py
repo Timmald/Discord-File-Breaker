3b874fce4fe52a2ed4a24de11af567d89672e4f2
Nathan Wolf <aprilscout.dog@gmail.com>
2022-03-06 19:37:27 -0500

    Changed app support folder

--- GlobalVars.py
+++ GlobalVars.py
@@ -1,7 +1,10 @@
 import json
 from pathlib import Path
+import os
+
 homeDir = str(Path.home())  # homeDir, for example, is /Users/nathanwolf
-appSupportFolder = f'{homeDir}/Library/Application Support/FileBreakerApp'
+appSupportFolder = f'/Library/Application Support/FileBreakerApp'
+#TODO: This is the only apps upport folder everyone has, yet I don't have perms to write there I don't think?
 currentChoicesPath = f'{appSupportFolder}/currentChoices.json'
 userInfoPath = f'{appSupportFolder}/userInfo.json'
 botChannelID = 939234547906777139
